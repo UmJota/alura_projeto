626aca2ceb0f761a53023925921a34540f10dbca
Jota <jpedrost90@gmail.com>
2024-09-07 23:58:40 -0300

    arrumando o main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,28 +1,24 @@
+function info() {
+    alert("Você terá que descobrir uma das escolas da Alura com alguns chutes. A cada chute serão mostradas as informações que você acertou e as que você errou para ficar mais fácil no seu próximo chute.");
+}
+
 function pesquisar() {
-    // Obtém a seção onde os resultados serão exibidos
     let section = document.getElementById("aluraquiz__resultados");
-
-    // Pega o valor do input de pesquisa e converte para letras minúsculas
     let placeHolder = document.getElementById("placeHolder").value;
-    const minLength = 2; // Define o número mínimo de caracteres exigido para a pesquisa
-    placeHolder = placeHolder.toLowerCase(); // Converte o valor inserido para letras minúsculas
-    let titulo = ""; // Variável que armazenará os títulos dos dados durante a comparação
-    let resultados = ""; // Variável que armazenará o HTML gerado para os resultados
+    const minLength = 2;
+    placeHolder = placeHolder.toLowerCase();
+    let titulo = "";
+    let resultados = "";
 
-    // Verifica se o valor inserido no input é menor que o mínimo exigido
+    // Verifica o número mínimo de caracteres
     if (placeHolder.length < minLength) {
-        alert(`Por favor, insira pelo menos ${minLength} letras para pesquisar.`); // Exibe um alerta se o input for muito curto
-        return; // Interrompe a execução da função se a condição não for atendida
+        alert(`Por favor, insira pelo menos ${minLength} letras para pesquisar.`);
+        return; // Interrompe a função se a condição não for atendida
     }
 
-    // Itera sobre o array 'dados' (presumido que seja uma lista de objetos com títulos e outras informações)
     for (let dado of dados) {
-        // Converte o título atual para letras minúsculas para facilitar a comparação
         titulo = dado.titulo.toLowerCase();
-
-        // Verifica se o valor do input (placeHolder) está contido no título
         if (titulo.includes(placeHolder)) {
-            // Se houver correspondência, adiciona o HTML correspondente ao resultado
             resultados += 
                 `
                     <tr>
@@ -53,10 +49,14 @@ function pesquisar() {
                         </th>
                     </tr>
                 `;
-            // Exibe um alerta com o resultado encontrado (provavelmente outro campo de dado)
             alert(`${dado.resultado}`);
         }
     }
-    // Insere o HTML gerado (resultados) dentro da seção de resultados no documento
+
+    // Verifica se há resultados e mostra a mensagem "Nada foi encontrado" se não houver.
+    if (!resultados) {
+        resultados = "<p>Nada foi encontrado</p>";
+    }
+
     section.innerHTML = resultados;
 }
